aa88a7d1f66070adedffd5b671411e1ef6764767
shufeng li <lsfhouse@gmail.com>
2020-04-10 23:39:00 -0500

    ICP3

---
+++ M2_ICP3/Source/bonusQuestion1.py
@@ -0,0 +1,70 @@
+from sklearn.datasets import fetch_20newsgroups
+from keras.models import Sequential
+from keras import layers
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Apply the code on 20_newsgroup data set we worked in the previous classes
+newsgroups_train = fetch_20newsgroups(subset='train', shuffle=True)  # categories=categories
+sentences = newsgroups_train.data
+y = newsgroups_train.target
+
+print(y.shape)
+
+unique, counts = np.unique(y, return_counts=True)
+print(dict(zip(unique, counts)))
+
+# tokenizing data
+tokenizer = Tokenizer(num_words=2000)
+tokenizer.fit_on_texts(sentences)
+max_review_len = max([len(s.split()) for s in sentences])
+print('Max review length: ', max_review_len)
+vocab_size = len(tokenizer.word_index) + 1
+print('vocab_size: ', vocab_size)
+# getting the vocabulary of data
+sentences = tokenizer.texts_to_matrix(sentences)
+padded_docs = pad_sequences(sentences, maxlen=max_review_len)
+
+le = preprocessing.LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25, random_state=1000)
+
+print('X_train.shape: ', X_train.shape)
+# Number of features
+# print(input_dim)
+dimData = np.prod(X_train.shape[1:])
+print('input_dim', dimData)
+
+# creating network
+np.random.seed(1)
+model = Sequential()
+model.add(layers.Dense(300, input_dim=dimData, activation='relu'))
+model.add(layers.Dense(20, activation='softmax'))
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=5, verbose=True, validation_data=(X_test, y_test), batch_size=256)
+
+# Print the evaluation result
+[test_loss, test_acc] = model.evaluate(X_test, y_test)
+print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
+
+# plot the loss for both training data and validation data.
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Loss for both training data and validation data')
+plt.xlabel('Number of epochs')
+plt.ylabel('Loss')
+plt.legend(['train', 'test'], loc='upper right')
+plt.show()
+
+# plot the accuracy for both training data and validation data.
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Accuracy for both training data and validation data')
+plt.xlabel('Number of epochs')
+plt.ylabel('Accuracy')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
---
+++ M2_ICP3/Source/task1.py
@@ -0,0 +1,55 @@
+from keras.models import Sequential
+from keras import layers
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+df = pd.read_csv('imdb_master.csv', encoding='latin-1')
+print(df.head())
+sentences = df['review'].values
+y = df['label'].values
+print(y.shape)
+# (100000,)
+unique, counts = np.unique(y, return_counts=True)
+print(dict(zip(unique, counts)))
+# {'neg': 25000, 'pos': 25000, 'unsup': 50000}
+
+# tokenizing data
+tokenizer = Tokenizer(num_words=2000)
+tokenizer.fit_on_texts(sentences)
+max_review_len = max([len(s.split()) for s in sentences])
+print('Max review length: ', max_review_len)
+vocab_size = len(tokenizer.word_index) + 1
+print('vocab_size: ', vocab_size)
+
+# getting the vocabulary of data
+sentences = tokenizer.texts_to_matrix(sentences)
+padded_docs = pad_sequences(sentences, maxlen=max_review_len)
+
+le = preprocessing.LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25, random_state=1000)
+
+print('X_train.shape: ', X_train.shape)
+# Number of features
+# print(input_dim)
+dimData = np.prod(X_train.shape[1:])
+print('input_dim: ', dimData)
+# input_dim:  2000
+
+# creating network
+np.random.seed(1)
+model = Sequential()
+model.add(layers.Dense(300, input_dim=dimData, activation='relu'))
+model.add(layers.Dense(3, activation='softmax'))
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=5, verbose=True, validation_data=(X_test, y_test),
+                    batch_size=256)
+
+# Print the evaluation result
+[test_loss, test_acc] = model.evaluate(X_test, y_test)
+print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
+# Evaluation result on Test Data : Loss = 1.054079183769226, accuracy = 0.5067200064659119
